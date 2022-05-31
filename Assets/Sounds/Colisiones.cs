94d10479769eb6702fb41810baa2b194d465e7b3
UrielJolodovsky <101568983+UrielJolodovsky@users.noreply.github.com>
2022-05-31 08:40:15 -0300

    Initial commit

---
+++ Assets/Sounds/Colisiones.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colisiones : MonoBehaviour {
+
+    public AudioClip telefono;
+    AudioSource fuenteaudio;
+
+	// Use this for initialization
+	void Start ()
+    {
+        fuenteaudio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            fuenteaudio.clip = telefono;
+            fuenteaudio.Play();
+        }
+         
+	}
+
+}
